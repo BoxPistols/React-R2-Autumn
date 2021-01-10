b1a55a906bffc0047c8e7a835e830922fe87fb5e
A_ito <BoxPistols@users.noreply.github.com>
2021-01-10 22:18:28 +0900

    rem Multi on Hook/Basic because this don't move

--- src/hooks/basic.jsx
+++ src/hooks/basic.jsx
@@ -7,43 +7,39 @@
  * useRef
  */
 import React, { useState, useEffect } from 'react'
-import Multi from './multi'
+// import Multi from './multi'
 
 function HookBasic() {
-  /*
-   * count => state
-   * setCount => 関数
-   * useState() 初期値
-   */
-  const [count, setCount] = useState(12)
-  const reset = () => setCount(0)
-  /*
-   * 副作用を発生させる、描画後に実行される
-   * countが更新されるたびに再描画出来る
-   */
-  useEffect(() => {
-    document.getElementById(
-      'effectHook'
-    ).innerText = `You Clicked ${count} times`
-  }, [count])
+    /*
+     * count => state
+     * setCount => 関数
+     * useState() 初期値
+     */
+    const [count, setCount] = useState(12)
+    const reset = () => setCount(0)
+    /*
+     * 副作用を発生させる、描画後に実行される
+     * countが更新されるたびに再描画出来る
+     */
+    useEffect(() => {
+        document.getElementById(
+            'effectHook'
+        ).innerText = `You Clicked ${count} times`
+    }, [count])
 
-  return (
-    <div>
-      {/* <p>Count: {count}</p> */}
-      <p id="effectHook"></p>
-      {/* setCountに引数を渡して実行 */}
-      <button
-        onClick={() => setCount((preCount) => preCount + 1)}
-      >
-        {' '}
-        Click!
-      </button>
-      <button onClick={reset}>Reset</button>
-      <div className="section">
-        <Multi />
-      </div>
-    </div>
-  )
+    return (
+        <div>
+            {/* <p>Count: {count}</p> */}
+            <p id='effectHook'></p>
+            {/* setCountに引数を渡して実行 */}
+            <button onClick={() => setCount((preCount) => preCount + 1)}>
+                {' '}
+                Click!
+            </button>
+            <button onClick={reset}>Reset</button>
+            <div className='section'>{/* <Multi /> */}</div>
+        </div>
+    )
 }
 
 export default HookBasic
