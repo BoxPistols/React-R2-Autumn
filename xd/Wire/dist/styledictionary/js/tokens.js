d11aa3ca0114d67ee388c4388c78c3fa0c8b7fc1
A_ito <BoxPistols@users.noreply.github.com>
2021-01-11 03:48:34 +0900

    sets XD (#2)
    Co-authored-by: ai <ai@ex.in>

---
+++ xd/Wire/dist/styledictionary/js/tokens.js
@@ -0,0 +1,14 @@
+/**
+ * Do not edit directly
+ * Generated on Sun, 10 Jan 2021 18:12:56 GMT
+ */
+
+export const 707070 = "#707070";
+export const 1Bbad3 = "#1bbad3"; // メインカラー
+export const Ffffff = "#ffffff";
+export const BizUdpゴシック20PtFamily = "BIZ UDPGothic";
+export const BizUdpゴシック20PtWeight = "Regular";
+export const メイリオ20PtFamily = "Meiryo";
+export const メイリオ20PtWeight = "Regular";
+export const BizUdpゴシック20PtSize = "20rem";
+export const メイリオ20PtSize = "20rem";
